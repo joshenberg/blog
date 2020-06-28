8fa4c7de401fcbe98f0285cc55ec5a1233e49359
Josh Rothenberg <joshua.rothenberg@coxautoinc.com>
2020-06-28 17:28:01 -0400

    Updates

---
+++ precache-manifest.6db0b562bef8107cc148930964dde024.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18613d070e9d68c74f62dfc7c5c66bdd",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "85b70fd40c213d422f46",
+    "url": "/blog/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0113ecf3e1fec608fe89",
+    "url": "/blog/static/js/2.b0b37a2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blog/static/js/2.b0b37a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b70fd40c213d422f46",
+    "url": "/blog/static/js/main.e4c9b728.chunk.js"
+  },
+  {
+    "revision": "042e0e1222fb6f3edc08",
+    "url": "/blog/static/js/runtime-main.1dc37962.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blog/precache-manifest.da8c6aa8530072ccdb13717e886fd34a.js"
+  "/blog/precache-manifest.6db0b562bef8107cc148930964dde024.js"
 );
 
 self.addEventListener('message', (event) => {
